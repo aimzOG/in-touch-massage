33d4579a2794e73da3a1257d2b2e37c198cd542b
aimzOG <aimz93@gmail.com>
2019-05-22 00:29:48 -0700

    adds FAQ page and skip nav

---
+++ dist/all.js
@@ -0,0 +1,1 @@
+var i,acc=document.getElementsByClassName("accordion");for(i=0;i<acc.length;i++)acc[i].onclick=function(){this.classList.toggle("active"),this.nextElementSibling.classList.toggle("show")};
