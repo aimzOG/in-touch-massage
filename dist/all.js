98ea64e33a1f90dd1f748f90306b18c5498c78d4
aimzOG <aimz93@gmail.com>
2019-05-22 17:40:50 -0700

    toggle's active accordion

--- dist/all.js
+++ dist/all.js
@@ -1,1 +1,1 @@
-var i,acc=document.getElementsByClassName("accordion");for(i=0;i<acc.length;i++)acc[i].onclick=function(){this.classList.toggle("active"),this.nextElementSibling.classList.toggle("show")};
+var i,j,acc=document.getElementsByClassName("accordion"),handleAccordionClick=function(){for(j=0;j<acc.length;j++)acc[j].classList.remove("active"),acc[j].nextElementSibling.classList.remove("show");this.classList.add("active"),this.nextElementSibling.classList.add("show")};for(i=0;i<acc.length;i++)acc[i].onclick=handleAccordionClick;
--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
 /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
 var acc = document.getElementsByClassName("accordion");
 var i;
+var j;
+
+var handleAccordionClick = function () {
+  for (j = 0; j < acc.length; j++) {
+    acc[j].classList.remove("active");
+    acc[j].nextElementSibling.classList.remove("show");
+  }
+  this.classList.add("active");
+  this.nextElementSibling.classList.add("show");
+}
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function () {
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("show");
-  }
+  acc[i].onclick = handleAccordionClick;
 }
